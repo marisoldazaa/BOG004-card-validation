807064a230abd4f11f454acfa10ce8d0f8439849
Marisol Daza <marisol.dazaa@gmail.com>
2022-02-03 13:13:10 -0500

    inicio JS

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 import validator from './validator.js';
 
-console.log(validator);
+function obtenerdatos()
+{alert("Le has dado click"); 
+
+var tarjetanumero = document.getElementsByClassName("inputnumber").value;
+console.log(tarjetanumero)
+}
+
+const boton = document.getElementById("pageone__button");//
+console.log(boton);
+// Uso el querySelector para que al dar click Devuelve el primer elemento encontrado en el documento//
+boton.addEventListener("click", obtenerdatos);
+    
--- src/page2.js
+++ src/page2.js
@@ -7,9 +7,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Validador</title>
     <link rel="stylesheet" href="style.css" />
-    <link rel="Homepage" href="index.html" />
+    <!-- <link rel="Homepage" href="src\index.html" /> -->
     <!-- Pusiste esta parte por qué quieres que se cree una conexión entre esta parte y el HTML inicial, 
     junto con el CSS inicial-->
+
 </head>
 
 <body>
@@ -22,11 +23,14 @@
         <h2 class="pagetwo__subtitle"> Ingresa el número de tu tarjeta</h2>
 
 <p>
-<input type="number" name="number">
+<input type="number" name="inputnumberr" class="inputnumber" placeholder="Ingresa tu numero de tarjeta" required>
+<!-- Placeholder: lo usamos para las letras que va sobre el campo a llenar -->
+    <!--value sirve para saber cual fue el dato que me envio el usuario cuando lo envio -->
+    <!-- required ayuda a que sea un campo obligatorio  -->
 </p>
         <p>
-            <button class="pagetwo__button" type="button">
-                <a href="C:/Users/Hp/Desktop/Laboratoria%202022/BOG004-card-validation/src/index3.html"
+            <button class="pagetwo__button" id="pagetwo__button" type="button" name="pagetwo__button">
+                <a href="index3.html"
                 title="Iniciar validación">VALIDAR</a>
             </button>
             <!-- Recuerda que es mejor usar Button porque este se puede modificasr en el HTML-->
@@ -49,6 +53,7 @@
 
         </header> -->
 
+         <script src="index.js" type="module"></script> 
 </body>
 
 </html>
