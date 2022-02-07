b21eaf685398ed6175659eec3be78e0e29e94012
Marisol Daza <marisol.dazaa@gmail.com>
2022-02-06 21:03:48 -0500

    moviendo los datos

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import validator from './validator.js';
 
-function obtenerdatos()
-{alert("Le has dado click"); 
+function obtenerdatos(){
+let arrayobtenerdatos = new Array (); //Genere un array para poder dividir los elementos obtenidos
+arrayobtenerdatos = document.getElementById("inputnumber").value; //Aquí estoy vinculando el html con el js
+{alert("Iniciar la validación"); 
+validator.isValid(arrayobtenerdatos) //Relacionar al validador con el js y validador 
+console.log(arrayobtenerdatos) //Evidenciar los números al ser digitados 
+}}
 
-var tarjetanumero = document.getElementsByClassName("inputnumber").value;
-console.log(tarjetanumero)
-}
 
-const boton = document.getElementById("pageone__button");//
+//{alert("Iniciar la validación"); 
+//var tarjetanumero = document.getElementById("inputnumber").value;
+//console.log(tarjetanumero)
+//validator.isValid(tarjetanumero)
+//}
+
+const boton = document.getElementById("pagetwo__button");//
 console.log(boton);
 // Uso el querySelector para que al dar click Devuelve el primer elemento encontrado en el documento//
 boton.addEventListener("click", obtenerdatos);
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,64 @@
 const validator = {
-  // ...
-};
+  isValid: function (arrayobtenerdatos){
+  //Esta función lo que hace es relacionar el indexjs con el validadorjs 
+console.log ("estoy en Valid")
+
+//Aquí estoy intentando separar los números para que sean caracteres independientes 
+var datosindividuales = Array.from(arrayobtenerdatos);
+console.log(datosindividuales)
+const reversed = datosindividuales.reverse();
+console.log('datosindividuales:', datosindividuales);
+
+// Intentar enmascarar los números
+
+function maskify(arrayobtenerdatos){
+  //if (arrayobtenerdatos.length < 12) return arrayobtenerdatos;
+	const cuatroUltimosNumeros = arrayobtenerdatos.substr(-4);
+	const primerosNumeros = arrayobtenerdatos
+	.substr(1, arrayobtenerdatos.length - 5)
+  .replace(/\d/g, '#');
+	return `${primerosNumeros}${cuatroUltimosNumeros}`;
+  console.log(maskify)
+}
+
+// Intentar sumar los elementos de un array 
+
+
+// este intento lo unico que hizo fue contar la cantidad de los elementos del array 
+//const sumar = [datosindividuales];
+//let sum = 0; 
+//for (let i=0; i <sumar.length; i++) {
+//sum += sumar[i];
+//}
+//console.log(sumar);
+//SI FUNCIONO 
+
+
+// vamos con el intento 2 de sumar // 
+
+//const sumando = [datosindividuales]
+//const reducer = (accumulator, curr) => accumulator + curr;
+//console.log(sumando.reduce(reducer));
+
+
+// vamos como el intento 3 
+
+//var arreglo = [arrayobtenerdatos];
+//var sumando =0;
+//for ( var i = 0; i < 4; i++){
+//arreglo [i]= (arrayobtenerdatos);
+  //arreglo [i] = Number (prompt ("ingresa"));
+ //sumando =  sumando + arreglo[i];
+//
+//console.log(sumando)
+//}
+
+
+// VAMOS POR EL 4 INTENTO 
+var total = [datosindividuales].reduce(function(a, b){ return a + b; });
+console.log(total)
+
+
+}};
 
 export default validator;
